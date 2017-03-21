3136e2eb81b12f0095d689570c3047529b1892bd
rajademo <srajamanikandan@hotmail.com>
2017-03-21 16:36:14 -0400

    New product

---
+++ src/main/java/com/himtech/googlechart/Product.java
@@ -0,0 +1,30 @@
+package com.raja.appcontroller;
+
+public class Product {
+
+	private String strProductName;
+	private String strStartDate;
+	private String strEndDate;
+	
+	
+	public String getStrProductName() {
+		return strProductName;
+	}
+	public void setStrProductName(String strProductName) {
+		this.strProductName = strProductName;
+	}
+	public String getStrStartDate() {
+		return strStartDate;
+	}
+	public void setStrStartDate(String strStartDate) {
+		this.strStartDate = strStartDate;
+	}
+	public String getStrEndDate() {
+		return strEndDate;
+	}
+	public void setStrEndDate(String strEndDate) {
+		this.strEndDate = strEndDate;
+	}
+	
+	
+}
