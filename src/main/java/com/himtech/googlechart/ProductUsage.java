8184660d45aad91a1719d94a9b8b9a048f6b32ee
rajademo <srajamanikandan@hotmail.com>
2017-03-21 22:26:09 -0400

    New productUsage file

---
+++ src/main/java/com/himtech/googlechart/ProductUsage.java
@@ -0,0 +1,32 @@
+package com.himtech.googlechart;
+
+public class ProductUsage {
+
+	private String strProductName;
+	private int availableLicense;
+	private int usedLicense;
+	
+	public String getStrProductName() {
+		return strProductName;
+	}
+	public void setStrProductName(String strProductName) {
+		this.strProductName = strProductName;
+	}
+	public int getAvailableLicense() {
+		return availableLicense;
+	}
+	public void setAvailableLicense(int availableLicense) {
+		this.availableLicense = availableLicense;
+	}
+	public int getUsedLicense() {
+		return usedLicense;
+	}
+	public void setUsedLicense(int usedLicense) {
+		this.usedLicense = usedLicense;
+	}
+	
+	
+	
+	
+	
+}
